37e3c3d8cf52b92672717c62822a60a7e3bfd22d
xingfeiy <yxfly@hotmail.com>
2018-06-21 21:09:44 -0700

    Added search engine on the home page

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,39 @@ var client = new Client();
 // var objQuery = client.query().q({text:'test', title:'test'});
 // var myStrQuery = 'q=text:test&wt=json';
 
+router.get('/query/:category/:content', function(req, res, next) {
+  
+  var url = '';
+  if (req.params.category == 'All') {
+    url = 'http://chainmap.org:8983/solr/chainmap/select?fl=title,%20summary,%20category&q=search_content:'+ encodeURI(req.params.content) +'&wt=json';
+  } else {
+    url = 'http://chainmap.org:8983/solr/chainmap/select?fl=title,%20summary,%20category&q=category:'+ encodeURI(req.params.category) + '%20AND%20search_content:'+ encodeURI(req.params.content) +'&wt=json';
+  }
+  
+    client.get(url, function (data, response) {
+      var obj = JSON.parse(data);
+      res.send(obj);
+  });
+});
+
+router.get('/resource/company/:name', function(req, res) {
+  res.redirect('http://chainmap.org/resource/company/' + req.params.name)
+});
+
+router.get('/resource/ico/:name', function(req, res) {
+  //http://chainmap.org/resource/ICO/Bitcoin%20Green
+  res.redirect('http://chainmap.org/resource/ICO/' + req.params.name)
+});
+
+
+router.get('/resource/event/:name', function(req, res) {
+  res.redirect('http://chainmap.org/resource/event/' + req.params.name);
+});
+
+
+router.get('/resource/white_paper/:name', function (req, res, next) {
+  res.redirect('http://chainmap.org/resource/white_paper/' + req.params.name);
+});
 
 router.get('/page', function(req, res) {
     res.render('home', { title: 'Home' });
