cbbf75e89bcda484775401dfca099eb2d0d6a968
Mason Tian <mtian@princeton.edu>
2018-07-17 14:02:54 -0700

    iterate through articles

--- utils/key word extraction/keywords.py
+++ utils/key word extraction/keywords.py
@@ -1,15 +1,21 @@
 from summa.summarizer import summarize
+from summa import keywords
 import unicodedata
 from os import listdir
 
 
 def format_text(filename):
     with open(filename) as f:
-        s = unicodedata.normalize('NFC', f.read())
-        print(s)
+        try:
+            s = unicodedata.normalize('NFC', f.read())
+        except UnicodeDecodeError:
+            print("Unicode cannot be decoded for ", filename)
+            return ""
     return s
 
-x = listdir("../articles/")
-print(x)
-#format_text()
-#summarize(s)
+folder = "../articles/"
+directory = listdir(folder)
+for filename in directory:
+    text = format_text(folder + filename)
+    kw = keywords.keywords(text)
+    print("keywords for ", folder + filename, " are ", kw)
