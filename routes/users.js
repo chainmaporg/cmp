741655bf20d067d6edd7c7e6912982564b1cebf0
Mason Tian <mtian@princeton.edu>
2018-08-09 14:54:58 -0700

    added link display, fixed mysql query

--- routes/users.js
+++ routes/users.js
@@ -561,7 +561,6 @@ exports.checkDuplicatePublicID = function(req, res) {
 
 
 exports.userProfile = (req, res) => {
-
     // user access their own profile while logged in
     if (req.params.user_id == req.session.user_id && req.session.user_id != null)
     {
@@ -571,7 +570,7 @@ exports.userProfile = (req, res) => {
     else if (req.params.user_id != null) {
         if (isNaN(req.params.user_id)) {
             connection.query(
-                "select connections.user_id, show_profile from connections inner join user on user.user_name = ?",
+                "select user.user_id, show_profile from connections inner join user on user.user_id=connections.user_id and user.user_name = ?",
                 [req.params.user_id],
                 (error, results, fields) => {
                     if (error) {
