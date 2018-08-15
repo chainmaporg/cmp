32092adfd0e94ce506051abfaabe96d59d131513
Mason Tian <mtian@princeton.edu>
2018-08-15 11:34:27 -0700

    updated randwalk code, minor reformatting

--- app.js
+++ app.js
@@ -1,99 +1,99 @@
-var createError = require('http-errors');
-var express = require('express');
-var session = require('express-session');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var fs = require('fs');
-var mysql = require('mysql');
-
+var createError = require("http-errors");
+var express = require("express");
+var session = require("express-session");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var fs = require("fs");
+var mysql = require("mysql");
 
 var logIP = function(req, res) {
     if (!req.session.logged) {
         console.log("New unique session.");
         console.log(req.connection.remoteAddress);
         req.session.logged = true;
-        connection.query(
-            "INSERT INTO ip (ip, timestamp) VALUES (?, NOW())",
-            [req.connection.remoteAddress],
-            function(error, results, fields) {
-                if (error) console.log("Error: ", error);
-            }
-        );
+        connection.query("INSERT INTO ip (ip, timestamp) VALUES (?, NOW())", [req.connection.remoteAddress], function(
+            error,
+            results,
+            fields,
+        ) {
+            if (error) console.log("Error: ", error);
+        });
     }
 };
-
-
 
 // var usersRouter = require('./routes/users');
 
 //var configFile = '../' + require('os').hostname() + '.js';
 
-serverConfigFile = "../cmap-2-0-config.js"
-localConfigFile = './local-deploy.js';
-console.log(">>Check your local config file and DB settings under ./local-deploy.js)",  localConfigFile)
+serverConfigFile = "../cmap-2-0-config.js";
+localConfigFile = "./local-deploy.js";
+console.log(">>Check your local config file and DB settings under ./local-deploy.js)", localConfigFile);
 
 configFile = fs.existsSync(serverConfigFile) ? serverConfigFile : localConfigFile;
 
-console.log(">>The server is picking this configuration:",  configFile)
+console.log(">>The server is picking this configuration:", configFile);
 
 var config = require(configFile);
 global.config = config;
 
 //load chain service
-require('./libs/ChainService');
+require("./libs/ChainService");
 
 //create db connection one time
-var startMysqlConnection=function(){
-	global.connection = mysql.createConnection(config.db);
-	connection.connect(function (err) {
-		if (err) {
-			console.log('error when connecting to db:', err);
-			// We introduce a delay before attempting to reconnect,
-			setTimeout(handleDisconnect, 2000);
-		}
-	});
-	connection.on('error', function (err) {
-		console.log('db error', err);
-		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-			startMysqlConnection();
-		} else {
-			throw err;
-		}
-	});
-}
+var startMysqlConnection = function() {
+    global.connection = mysql.createConnection(config.db);
+    connection.connect(function(err) {
+        if (err) {
+            console.log("error when connecting to db:", err);
+            // We introduce a delay before attempting to reconnect,
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+    connection.on("error", function(err) {
+        console.log("db error", err);
+        if (err.code === "PROTOCOL_CONNECTION_LOST") {
+            startMysqlConnection();
+        } else {
+            throw err;
+        }
+    });
+};
 startMysqlConnection();
 
-
-require('./libs/UserBalanceService');
-var indexRouter = require('./routes/index');
+require("./libs/UserBalanceService");
+var indexRouter = require("./routes/index");
 
 var app = express();
-app.use(session({
-	secret: 'chainmapsecret',
-	resave: false,
-	saveUninitialized: true
-}));
+app.use(
+    session({
+        secret: "chainmapsecret",
+        resave: false,
+        saveUninitialized: true,
+    }),
+);
 
-console.log("Requiring output graph")
-var outputGraph = require('./utils/outputGraph');
-outputGraph.getGraph();
-outputGraph.runPPR();
-outputGraph.getMappings();
+const outputGraph = require("./utils/outputGraph");
+const id_dict = outputGraph.getGraph();
+id_dict
+    .then(data => {
+        outputGraph.runPPR();
+        outputGraph.getMappings(data);
+    })
+    .catch(error => {});
 
 // middleware to make 'user' available to all templates
 
 app.use(function(req, res, next) {
-  res.locals.session = req.session;
-  logIP(req, res);
-  next();
+    res.locals.session = req.session;
+    logIP(req, res);
+    next();
 });
 
 // app.set('view engine', 'pug');
-app.set('view engine', 'ejs');
-app.use(express.static('public'))
-app.use(logger('dev'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -101,10 +101,9 @@ app.use(cookieParser());
 // app.use('/img',express.static(path.join(__dirname, 'public/images')));
 // app.use('/js',express.static(path.join(__dirname, 'public/javascripts')));
 // app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
-app.use(express.static('image'));
+app.use(express.static("image"));
 
-
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -114,21 +113,21 @@ app.use(function(req, res, next) {
 });
 **/
 // error handler
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	console.log("app:error -", err.message)
-	//res.locals.error = req.app.get('env') === 'development' ? err : {};
-	res.locals.error = err
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    console.log("app:error -", err.message);
+    //res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = err;
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error', { errorMsg: err });
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error", { errorMsg: err });
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(function (req, res, next) {
-	res.status(404).render('notFound', { title: "Sorry, page not found" });
+app.use(function(req, res, next) {
+    res.status(404).render("notFound", { title: "Sorry, page not found" });
 });
 module.exports = app;
--- utils/outputGraph.js
+++ utils/outputGraph.js
@@ -1,52 +1,67 @@
 exports.getGraph = () => {
-    connection.query("select distinct user_id, doc_id from click", (error, results, fields) => {
-        if (error) console.log(error);
-        else {
-            let graph_node_id = 0;
-            const get_graph_node_id_dict = {};
-            const get_hash_id_dict = {};
+    return new Promise((resolve, reject) => {
+        connection.query("select distinct user_id, doc_id from click", (error, results, fields) => {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                let graph_node_id = 0;
+                const get_graph_node_id_dict = {};
+                const get_hash_id_dict = {};
 
-            // find unique hash ids and user ids
-            const users = new Set();
-            const docs = new Set();
-            results.forEach(dict => {
-                users.add(dict.user_id);
-                docs.add(dict.doc_id);
-            });
+                // find unique hash ids and user ids
+                const users = new Set();
+                const docs = new Set();
+                results.forEach(dict => {
+                    users.add(dict.user_id);
+                    docs.add(dict.doc_id);
+                });
 
-            // assign consecutive ints as graph node ids
-            users.forEach(user_id => {
-                get_graph_node_id_dict[user_id] = graph_node_id;
-                get_hash_id_dict[graph_node_id] = user_id;
-                graph_node_id++;
-            });
+                // assign consecutive ints as graph node ids
+                users.forEach(user_id => {
+                    get_graph_node_id_dict[user_id] = graph_node_id;
+                    get_hash_id_dict[graph_node_id] = user_id;
+                    graph_node_id++;
+                });
 
-            docs.forEach(doc_id => {
-                get_graph_node_id_dict[doc_id] = graph_node_id;
-                get_hash_id_dict[graph_node_id] = doc_id;
-                graph_node_id++;
-            });
+                docs.forEach(doc_id => {
+                    get_graph_node_id_dict[doc_id] = graph_node_id;
+                    get_hash_id_dict[graph_node_id] = doc_id;
+                    graph_node_id++;
+                });
 
-            // output the graph
-            let output = "";
-            results.forEach(dict => {
-                output += get_graph_node_id_dict[dict.user_id] + " " + get_graph_node_id_dict[dict.doc_id] + "\n";
-            });
+                // output the graph
+                let output = "";
+                results.forEach(dict => {
+                    output += get_graph_node_id_dict[dict.user_id] + " " + get_graph_node_id_dict[dict.doc_id] + "\n";
+                    output += get_graph_node_id_dict[dict.doc_id] + " " + get_graph_node_id_dict[dict.user_id] + "\n";
+                });
 
-            console.log(output);
+                console.log(output);
 
-            const fs = require("fs");
-            fs.writeFile("./utils/graph.txt", output, err => {
-                if (err) {
-                    return console.log(err);
-                }
+                const fs = require("fs");
 
-                console.log("The file was saved!");
-            });
-            return get_hash_id_dict;
-        }
+                fs.writeFile("./utils/graph.txt", output, err => {
+                    if (err) {
+                        return console.log(err);
+                    }
+
+                    console.log("The file was saved!");
+                });
+
+                const params = "" + users.size + " " + docs.size;
+
+                fs.writeFile("./utils/params.txt", params, err => {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log("Params were saved!");
+                });
+
+                resolve([users.size, docs.size, get_hash_id_dict]);
+            }
+        });
     });
-    return {};
 };
 
 exports.runPPR = () => {
@@ -54,13 +69,17 @@ exports.runPPR = () => {
 
     console.log("running ppr executable.");
 
-    exec("./utils/randwalk", (err, data) => {
-        console.log(err);
-        console.log(data.toString());
+    exec("./utils/randwalk", (error, data) => {
+        if (error) {
+            console.log(err);
+        } else {
+            console.log(data.toString());
+        }
     });
 };
 
-exports.getMappings = () => {
+exports.getMappings = id_dict => {
+    console.log(id_dict);
     connection.query("select * from documents", (error, results, fields) => {
         if (error) console.log(error);
         // else console.log(results)
