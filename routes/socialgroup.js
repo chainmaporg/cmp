b73bfa803ec74b1a7c5609a9b3dc72a7b6adf0be
Mason Tian <mtian@princeton.edu>
2018-08-22 15:17:41 -0700

    partner page update

--- routes/socialgroup.js
+++ routes/socialgroup.js
@@ -1,32 +1,11 @@
-
-
-exports.getSocialGroup = function (req, res) {
-    console.log("Get All getSocialGroup")
-    var facebook_resultObj
-    var telegram_resultObj
-    var reddit_resultObj
-
-    
-
-    //telegram
-    connection.query('select * from socialgroup order by followers desc', [], function (error, result, fields) {
-        if (error) {
-            console.log("error ocurred", { title: 'Error on handling social group telegram' });
-            res.send({
-                "code": 400,
-                "failed": "error ocurred"
-            })
-            //res.render('error');
-        } else { 
-            res.render('connectGroup', { data: result})
-        }
-        
+exports.getSocialGroup = () => {
+    return new Promise((resolve, reject) => {
+        connection.query("select * from socialgroup order by followers desc", [], (error, results, fields) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
     });
-  
-
-
-}
-
-
-
-
+};
